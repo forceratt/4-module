ef9a4305563fdba823407fae227f8d56ee3d86d1
forceratt <vkudravceva33@gmail.com>
2025-06-24 19:00:49 +0300

    24.06

---
+++ javascripts/js-animation.js
@@ -0,0 +1,57 @@
+const decreaseBtn = document.getElementById("decrease");
+const increaseBtn = document.getElementById("increase");
+const valueSpan = document.getElementById("value");
+
+let value = 0;
+
+decreaseBtn.addEventListener("click", () => {
+  if (value > 0) {
+    value--;
+    valueSpan.textContent = value;
+  }
+});
+
+increaseBtn.addEventListener("click", () => {
+  if (value < 5) {
+    value++;
+    valueSpan.textContent = value;
+  }
+});
+const minusBtn = document.getElementById("minus-btn");
+const plusBtn = document.getElementById("plus-btn");
+const numberDisplay = document.getElementById("number-display");
+
+let number = 0;
+
+minusBtn.addEventListener("click", () => {
+  if (number > 0) {
+    number--;
+    numberDisplay.textContent = number;
+  }
+});
+
+plusBtn.addEventListener("click", () => {
+  if (number < 5) {
+    number++;
+    numberDisplay.textContent = number;
+  }
+});
+
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const modal = document.getElementById("myModal");
+
+openModalBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Закрытие при клике вне окна
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
